ace8ec239da7144bd3872b64e989922cd6b49a82
Brandon Riddle <brandonriddle828@gmail.com>
2024-10-31 09:10:17 -0400

    Add project files.

---
+++ PostApp/Controllers/MessageMicroservice.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GetApp2._0.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MessageController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            // Return a simple string message
+            return "Hello from the Microservice!";
+        }
+    }
+}
